4f5ea22feed26245458cd474d685a988f4ffb9ea
Zaruitoga <felixlesaulnier2005@gmail.com>
2025-04-07 13:56:25 +0200

    test numéros

--- renderer.js
+++ renderer.js
@@ -7,8 +7,22 @@ function openWhatsApp() {
 function sendMessage() {
     const contactList = [
         { name: "Alice", number: "33783215305" },
-        { name: "Bob", number: "33783215305" },
-        { name: "Charlie", number: "33783215305" }
+        { name: "Bob", number: "33769683329" },
+        { name: "Charlie", number: "33783215305" },
+        { name: "David", number: "33769683329" },
+        { name: "Eve", number: "33783215305" },
+        { name: "Frank", number: "33769683329" },
+        { name: "Grace", number: "33783215305" },
+        { name: "Heidi", number: "33769683329" },
+        { name: "Ivan", number: "33783215305" },
+        { name: "Judy", number: "33769683329" },
+        { name: "Mallory", number: "33783215305" },
+        { name: "Niaj", number: "33769683329" },
+        { name: "Olivia", number: "33783215305" },
+        { name: "Peggy", number: "33769683329" },
+        { name: "Rupert", number: "33783215305" },
+        { name: "Sybil", number: "33769683329" }
+    
     ];
     const message = "Bonjour {nom}, comment ça va ?";
     ipcRenderer.send('start-sending', message, contactList);
